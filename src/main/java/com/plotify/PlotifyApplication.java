c3b3d53501f756aec0c4edd0a23b750f6224d86a
Daniel Graham <danleeusa@gmail.com>
2016-09-03 17:57:13 -0400

    Initial Seed Commit

---
+++ src/main/java/com/plotify/PlotifyApplication.java
@@ -0,0 +1,12 @@
+package com.plotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlotifyApplication.class, args);
+	}
+}
